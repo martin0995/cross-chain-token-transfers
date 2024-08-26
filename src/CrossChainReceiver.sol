375fd1e3855de1cc03b3539b41d98fe9049a67aa
martin0995 <martin@papermoon.io>
2024-08-26 16:35:18 +0200

    receiver contract

--- src/CrossChainReceiver.sol
+++ src/CrossChainReceiver.sol
@@ -6,19 +6,17 @@ import "lib/wormhole-solidity-sdk/src/interfaces/IERC20.sol";
 
 contract CrossChainReceiver is TokenReceiver {
     // Constructor that passes the required arguments to the base contract
-    constructor(
-        address _wormholeRelayer,
-        address _tokenBridge,
-        address _wormhole
-    ) TokenBase(_wormholeRelayer, _tokenBridge, _wormhole) {}
+    constructor(address _wormholeRelayer, address _tokenBridge, address _wormhole)
+        TokenBase(_wormholeRelayer, _tokenBridge, _wormhole)
+    {}
 
     // Function to receive the cross-chain payload and tokens
     function receivePayloadAndTokens(
         bytes memory payload,
         TokenReceived[] memory receivedTokens,
         bytes32, // sourceAddress (not used in this implementation)
-        uint16,  // sourceChain (not used in this implementation)
-        bytes32  // deliveryHash (not used in this implementation)
+        uint16, // sourceChain (not used in this implementation)
+        bytes32 // deliveryHash (not used in this implementation)
     ) internal override onlyWormholeRelayer {
         require(receivedTokens.length == 1, "Expected 1 token transfer");
 
@@ -26,9 +24,6 @@ contract CrossChainReceiver is TokenReceiver {
         address recipient = abi.decode(payload, (address));
 
         // Transfer the received tokens to the intended recipient
-        IERC20(receivedTokens[0].tokenAddress).transfer(
-            recipient,
-            receivedTokens[0].amount
-        );
+        IERC20(receivedTokens[0].tokenAddress).transfer(recipient, receivedTokens[0].amount);
     }
 }
