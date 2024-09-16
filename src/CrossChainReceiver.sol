eafe372da54c62c09ff89e0cce255c6ddd5ea886
martin0995 <martin@papermoon.io>
2024-09-16 17:43:12 +0200

    updated contract to include emitter check

--- src/CrossChainReceiver.sol
+++ src/CrossChainReceiver.sol
@@ -5,19 +5,20 @@ import "lib/wormhole-solidity-sdk/src/WormholeRelayerSDK.sol";
 import "lib/wormhole-solidity-sdk/src/interfaces/IERC20.sol";
 
 contract CrossChainReceiver is TokenReceiver {
-    // Constructor that passes the required arguments to the base contract
+    // The wormhole relayer and registeredSenders are inherited from the Base.sol contract.
+
     constructor(address _wormholeRelayer, address _tokenBridge, address _wormhole)
         TokenBase(_wormholeRelayer, _tokenBridge, _wormhole)
     {}
 
-    // Function to receive the cross-chain payload and tokens
+    // Function to receive the cross-chain payload and tokens with emitter validation
     function receivePayloadAndTokens(
         bytes memory payload,
         TokenReceived[] memory receivedTokens,
-        bytes32, // sourceAddress (not used in this implementation)
-        uint16, // sourceChain (not used in this implementation)
+        bytes32 sourceAddress,
+        uint16 sourceChain,
         bytes32 // deliveryHash (not used in this implementation)
-    ) internal override onlyWormholeRelayer {
+    ) internal override onlyWormholeRelayer isRegisteredSender(sourceChain, sourceAddress) {
         require(receivedTokens.length == 1, "Expected 1 token transfer");
 
         // Decode the recipient address from the payload
